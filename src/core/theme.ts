ef9a41b83074cffb30bbd5b5d23bce9c1a9852fc
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-13 10:17:32 +0200

    added theme

---
+++ src/core/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    primary: "#0B0D17",
+    secondary: "#D0D6F9",
+    third: "#FFFFFF",
+    medium: "#979797",
+    light: "#F2F2F2",
+    dark: "#24262F",
+    transparentDark: "rgba(36, 38, 47, 0.6)",
+  },
+};
