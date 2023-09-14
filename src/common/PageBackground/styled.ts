6c29d3402dfbf337ea5613fcbc2c96623e2bb36c
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-14 10:17:42 +0200

    added styles for PageBackground common component

---
+++ src/common/PageBackground/styled.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+interface BackgroundProps {
+  bgdesktop: string;
+  bgtablet: string;
+  bgmobile: string;
+}
+
+export const Background = styled.div<BackgroundProps>`
+  background-image: url("${props => props.bgdesktop}");
+  min-height: 100vh;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  z-index: 2;
+  position: relative;
+
+  @media (max-width: 1024px) {
+    background-image: url("${props => props.bgtablet}");
+  }
+
+  @media (max-width: 767px) {
+    background-image: url("${props => props.bgmobile}");
+  }
+`;
