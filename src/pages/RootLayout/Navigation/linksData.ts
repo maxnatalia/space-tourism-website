af8ffbb20ff53583a67b1f4e719031f6b095a1c0
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-17 07:58:35 +0200

    added data for Navigation

---
+++ src/pages/RootLayout/Navigation/linksData.ts
@@ -0,0 +1,22 @@
+export const linksData = [
+  {
+    num: "00",
+    name: "Home",
+    to: "/",
+  },
+  {
+    num: "01",
+    name: "Destination",
+    to: "/destination",
+  },
+  {
+    num: "02",
+    name: "Crew",
+    to: "/crew",
+  },
+  {
+    num: "03",
+    name: "Technology",
+    to: "/technology",
+  },
+];
