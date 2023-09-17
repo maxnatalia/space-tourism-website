323898d4bba931b819d7970448c9112bc78cbd4b
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-17 07:58:22 +0200

    created Navigation common component

---
+++ src/pages/RootLayout/Navigation/index.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { ReactComponent as Logo } from "./logo.svg";
+import { ReactComponent as HamburgerIcon } from "./icon-hamburger.svg";
+import { ReactComponent as CloseIcon } from "./icon-close.svg";
+import { LinkName, LinkNum, LinksList, MobileView, StyledNav } from "./styled";
+import { linksData } from "./linksData";
+
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleToggleNav = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
+  return (
+    <StyledNav show={isOpen}>
+      <Logo />
+      <LinksList show={isOpen}>
+        {linksData.map(link => (
+          <li key={link.name}>
+            <LinkName to={link.to}>
+              <LinkNum>{link.num}</LinkNum>
+              {link.name}
+            </LinkName>
+          </li>
+        ))}
+      </LinksList>
+      <MobileView onClick={handleToggleNav}>
+        {isOpen ? <CloseIcon /> : <HamburgerIcon />}
+      </MobileView>
+    </StyledNav>
+  );
+};
+
+export default Navigation;
