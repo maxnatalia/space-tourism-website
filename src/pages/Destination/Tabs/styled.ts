0da90a9a93bf02a5f4aa3bb11251585a081ca5b3
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-18 08:17:15 +0200

    added styles for Tabs component

---
+++ src/pages/Destination/Tabs/styled.ts
@@ -0,0 +1,53 @@
+import styled, { css } from "styled-components";
+
+interface ActiveProps {
+  active: boolean;
+}
+
+export const Navigator = styled.div`
+  display: flex;
+
+  @media (max-width: 1024px) {
+    justify-content: center;
+    margin-bottom: 50px;
+  }
+
+  @media (max-width: 767px) {
+    margin-bottom: 26px;
+  }
+`;
+
+export const NavList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding-left: 0;
+  display: flex;
+  gap: 20px;
+`;
+
+export const NavItem = styled.li<ActiveProps>`
+  padding: 20px 0;
+  color: ${({ theme }) => theme.colors.secondary};
+  text-transform: uppercase;
+  font-family: "Barlow Condensed", sans-serif;
+  font-size: 16px;
+  letter-spacing: 2.7px;
+  border-bottom: 2px solid transparent;
+  cursor: pointer;
+  transition: border 0.3s ease-in;
+
+  &:hover {
+    ${({ active }) =>
+      !active &&
+      css`
+        border-bottom: 2px solid ${({ theme }) => theme.colors.medium};
+      `}
+  }
+
+  ${({ active }) =>
+    active &&
+    css`
+      border-bottom: 2px solid ${({ theme }) => theme.colors.third};
+      color: ${({ theme }) => theme.colors.third};
+    `}
+`;
