3d3420903298299085e80b2eb23063fd9918a22a
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-20 11:05:16 +0200

    updated styles - using props foe PageLayout

--- src/common/PageLayout/styled.ts
+++ src/common/PageLayout/styled.ts
@@ -1,9 +1,26 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 interface BackgroundProps {
   bgdesktop: string;
   bgtablet: string;
   bgmobile: string;
+}
+interface ReverseProps {
+  reverseRow?: boolean;
+  reverseCol?: boolean;
+}
+
+interface ReverseNavProps {
+  reverseNavCol?: boolean;
+  reverseNavRow?: boolean;
+}
+
+interface EndProps {
+  end?: boolean;
+}
+
+interface ImageProps {
+  smaller?: boolean;
 }
 
 export const Background = styled.div<BackgroundProps>`
@@ -26,26 +43,124 @@ export const Background = styled.div<BackgroundProps>`
 
 export const Section = styled.section`
   padding-top: 200px;
+  margin-bottom: 40px;
 
-  @media (max-width: 1024px) {
+  /* @media (max-width: 1024px) {
     padding-top: 134px;
   }
 
   @media (max-width: 767px) {
     padding-top: 90px;
-  }
+  } */
 `;
 
-export const Num = styled.span`
-  color: ${({ theme }) => theme.colors.medium};
-  font-weight: 900;
-  padding-left: 170px;
+export const TitleWrapper = styled.div`
+  padding-left: 168px;
 
   @media (max-width: 1024px) {
     padding-left: 50px;
   }
+`;
 
-  @media (max-width: 767px) {
-    padding-left: 0;
+export const Article = styled.article`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const ImageBox = styled.div<EndProps>`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${({ end }) =>
+    end &&
+    css`
+      justify-content: flex-end;
+
+      @media (max-width: 1024px) {
+        justify-content: center;
+      }
+    `}
+`;
+export const Navigator = styled.div``;
+export const Num = styled.span`
+  color: ${({ theme }) => theme.colors.medium};
+  font-weight: 900;
+`;
+
+export const Box = styled.div<ReverseProps>`
+  padding-left: 168px;
+  min-height: 700px;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 1024px) {
+    margin: 58px auto;
+    padding: 0 120px;
+    flex-direction: column;
+    /* gap: 140px; */
+    text-align: center;
   }
+
+  ${({ reverseRow }) =>
+    reverseRow &&
+    css`
+      padding-right: 160px;
+      flex-direction: row-reverse;
+      gap: 126px;
+    `}
+
+  ${({ reverseCol }) =>
+    reverseCol &&
+    css`
+      @media (max-width: 1024px) {
+        padding: 0 0;
+        flex-direction: column-reverse;
+        gap: 0;
+      }
+    `}
+`;
+
+export const Content = styled.div<ReverseNavProps>`
+  display: flex;
+  flex-direction: column;
+
+  ${({ reverseNavCol }) =>
+    reverseNavCol &&
+    css`
+      flex-direction: column-reverse;
+    `}
+
+  ${({ reverseNavRow }) =>
+    reverseNavRow &&
+    css`
+      flex-direction: row-reverse;
+      align-items: flex-start;
+      gap: 72px;
+
+      @media (max-width: 1024px) {
+        flex-direction: column-reverse;
+      }
+    `}
+`;
+
+export const Image = styled.img<ImageProps>`
+  filter: drop-shadow(
+    12px 2px 100px ${({ theme }) => theme.colors.transparentMedium}
+  );
+
+  ${({ smaller }) =>
+    smaller &&
+    css`
+      height: 445px;
+      width: 445px;
+
+      @media (max-width: 1024px) {
+        height: 300px;
+        width: 300px;
+      }
+    `}
 `;
