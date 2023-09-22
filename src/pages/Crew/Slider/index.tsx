320a218bf6f32a8bac74b33bb652f33852ed694d
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-22 10:05:30 +0200

    improved import paths for Slider

--- src/pages/Crew/Slider/index.tsx
+++ src/pages/Crew/Slider/index.tsx
@@ -1,5 +1,5 @@
 import { CrewMember } from "../../../core/useFetchData";
-import { ActiveItemProps } from "../../useActiveItem";
+import { ActiveItemProps } from "../../../core/useActiveItem";
 import { SliderPoint, StyledSlider } from "./styled";
 
 interface SliderProps extends ActiveItemProps {
