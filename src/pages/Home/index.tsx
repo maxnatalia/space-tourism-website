d2c7359bf39e4c9e7d4765065d304bf8aefdc898
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-23 08:35:48 +0200

    updated - added BackgroundPage and SectionLayout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom";
-import PageLayout from "../../common/PageLayout";
-import { HeadingFive } from "../../common/Heading";
+import BackgroundPage from "../../common/BackgroundPage";
+import SectionLayout from "../../common/SectionLayout";
+import { HeadingOne, HeadingFive } from "../../common/Heading";
 import { Paragraph } from "../../common/Paragraph";
 import bgdesktop from "./background-home-desktop.jpg";
 import bgmobile from "./background-home-mobile.jpg";
 import bgtablet from "./background-home-tablet.jpg";
-import { ExploreButton, GridBox, Heading, ContentBox } from "./styled";
+import { ExploreButton, Wrapper } from "./styled";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -15,21 +16,28 @@ const Home = () => {
   };
 
   return (
-    <PageLayout bgdesktop={bgdesktop} bgtablet={bgtablet} bgmobile={bgmobile}>
-      <GridBox>
-        <ContentBox>
+    <BackgroundPage
+      bgdesktop={bgdesktop}
+      bgtablet={bgtablet}
+      bgmobile={bgmobile}
+    >
+      <SectionLayout
+        extraButton={
+          <ExploreButton onClick={handleNavigate}>EXPLORE</ExploreButton>
+        }
+      >
+        <Wrapper>
           <HeadingFive>SO, YOU WANT TO TRAVEL TO</HeadingFive>
-          <Heading>space</Heading>
+          <HeadingOne>space</HeadingOne>
           <Paragraph>
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
             world experience!
           </Paragraph>
-        </ContentBox>
-        <ExploreButton onClick={handleNavigate}>EXPLORE</ExploreButton>
-      </GridBox>
-    </PageLayout>
+        </Wrapper>
+      </SectionLayout>
+    </BackgroundPage>
   );
 };
 
