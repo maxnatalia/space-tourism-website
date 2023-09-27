e27fefc855008160cc376ef83b2a9c6064fa9116
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-27 08:56:04 +0200

    updated - added crew and technology paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import Home from "./pages/Home";
 import Destination from "./pages/Destination";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
         <Route path="/" element={<RootLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
         </Route>
       </Routes>
     </BrowserRouter>
