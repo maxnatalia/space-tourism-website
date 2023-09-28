8c0bc41e98cff967597af37dea1b8d50ec3c828c
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-28 10:12:49 +0200

    implemented animations for Navigation

--- src/pages/RootLayout/Navigation/index.tsx
+++ src/pages/RootLayout/Navigation/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 import { ReactComponent as Logo } from "./logo.svg";
 import { ReactComponent as HamburgerIcon } from "./icon-hamburger.svg";
 import { ReactComponent as CloseIcon } from "./icon-close.svg";
@@ -7,6 +8,7 @@ import { linksData } from "./linksData";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const isDesktop = window.innerWidth > 767;
 
   const handleToggleNav = () => {
     setIsOpen(prevState => !prevState);
@@ -19,17 +21,45 @@ const Navigation = () => {
   return (
     <StyledNav show={isOpen}>
       <Logo />
-      <LinksList show={isOpen}>
+      <LinksList
+        variants={{
+          hidden: {
+            opacity: 0,
+            x: -50,
+          },
+          visible: {
+            opacity: 1,
+            x: 0,
+            transition: { staggerChildren: 0.5 },
+          },
+        }}
+        initial="hidden"
+        animate={isDesktop || isOpen ? "visible" : "hidden"}
+        exit={"hidden"}
+        show={isOpen}
+      >
         {linksData.map(link => (
-          <li key={link.name}>
+          <motion.li
+            variants={{
+              hidden: { opacity: 0, scale: 2 },
+              visible: { opacity: 1, scale: 1 },
+            }}
+            exit={{ opacity: 1, scale: 1 }}
+            transition={{ type: "spring" }}
+            key={link.name}
+          >
             <LinkName to={link.to} onClick={handleLinkClick}>
               <LinkNum>{link.num}</LinkNum>
               {link.name}
             </LinkName>
-          </li>
+          </motion.li>
         ))}
       </LinksList>
-      <MobileView onClick={handleToggleNav}>
+      <MobileView
+        whileTap={{ scale: 1.1 }}
+        transition={{ type: "spring", stiffness: 500 }}
+        onClick={handleToggleNav}
+      >
         {isOpen ? <CloseIcon /> : <HamburgerIcon />}
       </MobileView>
     </StyledNav>
