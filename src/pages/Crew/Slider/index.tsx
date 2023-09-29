89113ee1463f35ba00a906861f756bf281d10c6e
maxnatalia <Nataliamazur1988@gmail.com>
2023-09-29 08:40:22 +0200

    added animations for Slider in Crew page

--- src/pages/Crew/Slider/index.tsx
+++ src/pages/Crew/Slider/index.tsx
@@ -6,11 +6,23 @@ interface SliderProps extends ActiveItemProps {
   crew: CrewMember[];
 }
 
+const pointVariants = {
+  hidden: { opacity: 0, scale: 3, x: 200 },
+  visible: { opacity: 1, scale: 1, x: 0 },
+};
+
 const Slider = ({ crew, activeItem, handleChangeItem }: SliderProps) => {
   return (
-    <StyledSlider>
+    <StyledSlider
+      variants={{
+        hidden: { opacity: 0, x: -50 },
+        visible: { opacity: 1, x: 0, transition: { staggerChildren: 0.1 } },
+      }}
+    >
       {crew.map((member, index) => (
         <SliderPoint
+          variants={pointVariants}
+          transition={{ type: "spring" }}
           active={activeItem === index}
           key={member.name}
           onClick={() => handleChangeItem(index)}
