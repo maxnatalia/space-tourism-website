3d2cf1836a2513ab606abec5a1775e7c6eed15e6
maxnatalia <Nataliamazur1988@gmail.com>
2023-10-01 08:00:34 +0200

    added animations for SliderNum component

--- src/pages/Technology/SliderNum/index.tsx
+++ src/pages/Technology/SliderNum/index.tsx
@@ -6,15 +6,34 @@ interface SliderNumProps extends ActiveItemProps {
   technology: TechnologyItem[];
 }
 
+const numVariants = {
+  hidden: { opacity: 0, scale: 3, x: -200 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    x: 0,
+    transition: { duration: 2, delay: 1 },
+  },
+};
+
 const SliderNum = ({
   technology,
   activeItem,
   handleChangeItem,
 }: SliderNumProps) => {
   return (
-    <StyledSliderNum>
+    <StyledSliderNum
+      variants={{
+        hidden: { opacity: 0 },
+        visible: {
+          opacity: 1,
+          transition: { staggerChildren: 0.3 },
+        },
+      }}
+    >
       {technology.map((_, index) => (
         <NumPoint
+          variants={numVariants}
           onClick={() => handleChangeItem(index)}
           key={index}
           active={activeItem === index}
